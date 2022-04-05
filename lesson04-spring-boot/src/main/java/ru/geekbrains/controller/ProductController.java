91e1046aeff2b93e171666af67cc941bc9ccd2a2
Kom7oT <kom7ot@gmail.com>
2022-04-05 15:44:25 +0300

    Lesson 4 Spring Boot

---
+++ lesson04-spring-boot/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.persist.Product;
+import ru.geekbrains.persist.ProductRepository;
+
+import javax.naming.Binding;
+
+@RequestMapping("/products")
+@Controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String listPage(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable("id") long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+        return "product_form";
+    }
+
+    //Добавление товара
+    @GetMapping("/new")
+    public String form(Model model) {
+        model.addAttribute("product", new Product("", null));
+        return "product_form";
+    }
+
+    @PostMapping
+    public String save(Product product, BindingResult binding) {
+        productRepository.save(product);
+        return "redirect:/products";
+    }
+
+    //Удаление товара
+    @GetMapping("/delete")
+    public String delete(@RequestParam long id, Model model) {
+        productRepository.delete(id);
+        listPage(model);
+        return "products";
+    }
+}
