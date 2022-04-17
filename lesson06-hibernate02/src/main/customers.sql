fdc164b95718e61b85187b00a88c802b273b70fb
Kom7oT <86073918+Kom7oT@users.noreply.github.com>
2022-04-17 12:01:26 +0300

    Lesson 5 Hibernate pt.2

---
+++ lesson06-hibernate02/src/main/customers.sql
@@ -0,0 +1,9 @@
+INSERT INTO test.customers (id, name) VALUES (1, 'Jack');
+INSERT INTO test.customers (id, name) VALUES (2, 'Piter');
+INSERT INTO test.customers (id, name) VALUES (3, 'John');
+INSERT INTO test.customers (id, name) VALUES (4, 'Goodwin');
+INSERT INTO test.customers (id, name) VALUES (5, 'Bruce');
+INSERT INTO test.customers (id, name) VALUES (6, 'Levon');
+INSERT INTO test.customers (id, name) VALUES (7, 'Gevorg');
+INSERT INTO test.customers (id, name) VALUES (8, 'Anthony');
+INSERT INTO test.customers (id, name) VALUES (9, 'Vilat');
