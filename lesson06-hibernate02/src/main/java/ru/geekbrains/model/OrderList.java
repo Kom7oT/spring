cdffddd1c3b8f647268f43f4056c1989dccfd70a
Kom7oT <86073918+Kom7oT@users.noreply.github.com>
2022-04-17 12:08:59 +0300

    Lesson 5 Hibernate pt.2

---
+++ lesson06-hibernate02/src/main/java/ru/geekbrains/model/OrderList.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders_list")
+public class OrderList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Product product;
+
+    public OrderList() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+}
